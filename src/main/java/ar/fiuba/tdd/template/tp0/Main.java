6e4808fd78a7f3fd1e54bf2cac6a8c1cd82088cf
Francisco <plandino@hotmail.com>
2016-03-19 15:18:28 -0300

    Funcionalidades basicas terminadas. Hay que revisar todo y crear mas pruebas.

--- src/main/java/ar/fiuba/tdd/template/tp0/Main.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/Main.java
@@ -7,13 +7,14 @@ import java.util.List;
  */
 public class Main {
 
-    private static RegExGenerator generador = new RegExGenerator(5);
+    private static RegExGenerator generador = new RegExGenerator(3);
     private static List<String> listaDePalabras;
 
     public static void main(String[] args) {
 
-        listaDePalabras = generador.generate("..+[abcd]*a+bss?h*.", 3);
+        listaDePalabras = generador.generate(".....+[abcd]*a+bss?h*vara\\*asd*.+", 3);
         int size = listaDePalabras.size();
+
         for(int i = 0; i < size; i++){
             String palabra = listaDePalabras.get(i);
             System.out.println(palabra);
--- src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
@@ -7,11 +7,21 @@ import java.util.Random;
 public class RegExGenerator {
     // TODO: Uncomment this field
     private int maxLength;
+    final private int TAMANIO_ASCII = 256;
+
     private Random random;
+    private String CONTRA_BARRA = "\\";
+    private String PUNTO = ".";
+    private String auxString = "";
+
+    private boolean appendTokens = false;
+
+    private ArrayList<String> lista;
 
     public RegExGenerator( int maxLength ) {
         this.maxLength = maxLength + 1;     // Le sumo 1, porque en el Random no se incluye el valor limite.
         this.random = new Random();
+        this.lista = new ArrayList<>();
     }
 
     // TODO: Uncomment parameters
@@ -20,67 +30,108 @@ public class RegExGenerator {
         int cantidadDeOcurrencias;
         int minimaCantidadDeOcurrencias = 1;
 
-        ArrayList<String> lista = new ArrayList<>();
-        
-        String[] tokens = regEx.split("(?<=\\+)|(?<=\\*)|(?<=\\?)|(?=\\[)");
-
+        String[] tokens = regEx.split("(?=\\.)|(?<=\\+)|(?<=\\*)|(?<=\\?)|(?=\\[)");
 
         for( String token : tokens ){
+
+            if( appendTokens ) {
+                auxString += token;
+                token = auxString;
+                appendTokens = false;
+            }
+
             System.out.println("El token es: " + token);
 
-            int lastCharIndex = token.length() - 1;
-            System.out.println("Last char index: " + lastCharIndex );
+            int lastCharIndex = 1;
+            String lastChar = "";
 
-            String lastChar = token.substring( lastCharIndex );
-
-            switch ( lastChar ) {
-                case "?":
-
-                    boolean agregar = random.nextBoolean();
-
-                    if( agregar )
-                        lista.add( token.substring( 0, lastCharIndex ) );
-                    break;
-
-                case "*":
-
-                    cantidadDeOcurrencias = minimaCantidadDeOcurrencias + ( random.nextInt( maxLength - minimaCantidadDeOcurrencias ) );
-
-                    for( int i = 0; i < cantidadDeOcurrencias; i++ ) {
-                        lista.add( token.substring( 0, lastCharIndex ) );
-                    }
-                    break;
-
-                case "+":
-
-                    cantidadDeOcurrencias = 1 + ( random.nextInt( maxLength ) );
-
-                    for( int i = 0; i < cantidadDeOcurrencias; i++ ) {
-                        lista.add( token.substring( 0, lastCharIndex ) );
-                    }
-                    break;
+            if( ! token.equals( PUNTO ) ) {
+                lastCharIndex = token.length() - 1;
+                lastChar = token.substring( lastCharIndex );
             }
 
-            switch ( token ) {
+            System.out.println("lascharindex: " + lastCharIndex);
 
-                case "\\":
-                    lista.add("barra");
-                    break;
-                case "[":
-                    lista.add("corchete");
-                    break;
-                case ".":
-                    char caracter = ( char ) random.nextInt(255);
-                    lista.add( String.valueOf( caracter ) );
-                    break;
-                default:
-                    lista.add( token );
-                    break;
+            if( isBackSlashed(token, lastCharIndex) ) {
+                auxString = token.replace( CONTRA_BARRA, "" );
+                appendTokens = true;
+            } else {
+
+
+                token = token.substring( 0, lastCharIndex );
+                String set = null;
+
+                if( isASet( token ) ) {
+
+                    // TODO: Ver que no estan backslashed
+                    token = token.substring( token.indexOf( "[" ) + 1, token.lastIndexOf( "]" ) );
+                    set = token;
+                }
+                System.out.println("El token quedo: " + token);
+
+                switch ( lastChar ) {
+
+                    case "?":
+
+                        boolean agregar = random.nextBoolean();
+
+                        if( agregar )
+                            addCharacter( token, set );
+                        break;
+
+                    case "*":
+
+                        cantidadDeOcurrencias = minimaCantidadDeOcurrencias + ( random.nextInt( maxLength - minimaCantidadDeOcurrencias ) );
+
+                        for( int i = 0; i < cantidadDeOcurrencias; i++ ) {
+                            addCharacter( token, set );
+                        }
+                        break;
+
+                    case "+":
+
+                        cantidadDeOcurrencias = 1 + ( random.nextInt( maxLength ) );
+
+                        for( int i = 0; i < cantidadDeOcurrencias; i++ ) {
+                            addCharacter( token, set );
+                        }
+                        break;
+
+                    default:
+
+                        addCharacter( token, set );
+                        break;
+                }
             }
+
             System.out.println("Agregue: " + lista.get(lista.size() - 1));
-
         }
-
         return lista;
     }
+
+    private boolean isBackSlashed( String token, int lastCharIndex ) {
+        if( lastCharIndex > 0 ){
+            String anteUltimoChar = token.substring( lastCharIndex - 1, lastCharIndex);
+            return anteUltimoChar.equals( CONTRA_BARRA );
+        }
+        return false;
+    }
+
+    private void addCharacter( String token, String set) {
+        if( token.equals( PUNTO ) ) {
+            char caracter = ( char ) random.nextInt( TAMANIO_ASCII );
+            lista.add( String.valueOf( caracter ) );
+        } else if ( set != null ) {
+            char caracter = set.charAt( random.nextInt( set.length() ) );
+            lista.add( String.valueOf( caracter ) );
+        } else {
+            lista.add( token );
+        }
+    }
+
+    // TODO: ver que no esta backslashed
+    private boolean isASet( String token ) {
+        return token.contains( "[" );
+    }
+
 }
