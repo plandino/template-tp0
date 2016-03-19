deb89b48f2ca787ac1e9174b0520b16416b55c53
Francisco <plandino@hotmail.com>
2016-03-19 11:39:40 -0300

    Iniciando el desarrollo del tp. Commit para probar travis.

---
+++ src/main/java/ar/fiuba/tdd/template/tp0/Main.java
@@ -0,0 +1,29 @@
+package ar.fiuba.tdd.template.tp0;
+
+import java.util.List;
+
+/**
+ * Created by panchoubuntu on 18/03/16.
+ */
+public class Main {
+
+    private static RegExGenerator generador = new RegExGenerator(5);
+    private static List<String> listaDePalabras;
+
+    public static void main(String[] args) {
+
+        listaDePalabras = generador.generate("..+[abcd]*a+bss?h*.", 3);
+        int size = listaDePalabras.size();
+        for(int i = 0; i < size; i++){
+            String palabra = listaDePalabras.get(i);
+            System.out.println(palabra);
+        }
+//        listaDePalabras = generador.generate("*.l.s", 3);
+//        size = listaDePalabras.size();
+//        for(int i = 0; i < size; i++){
+//            String palabra = listaDePalabras.get(i);
+//            System.out.println(palabra);
+//        }
+        System.out.println("Goodbye, World!");
+    }
+}
--- src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
@@ -2,23 +2,85 @@ package ar.fiuba.tdd.template.tp0;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class RegExGenerator {
     // TODO: Uncomment this field
-    //private int maxLength;
+    private int maxLength;
+    private Random random;
 
-    //public RegExGenerator(int maxLength) {
-    //    this.maxLength = maxLength;
-    //}
+    public RegExGenerator( int maxLength ) {
+        this.maxLength = maxLength + 1;     // Le sumo 1, porque en el Random no se incluye el valor limite.
+        this.random = new Random();
+    }
 
     // TODO: Uncomment parameters
-    public List<String> generate(/*String regEx, int numberOfResults*/) {
-        return new ArrayList<String>() {
-            {
-                add("a");
-                add("b");
-                add("c");
+    public List<String> generate( String regEx, int numberOfResults ) {
+
+        int cantidadDeOcurrencias;
+        int minimaCantidadDeOcurrencias = 1;
+
+        ArrayList<String> lista = new ArrayList<>();
+        
+        String[] tokens = regEx.split("(?<=\\+)|(?<=\\*)|(?<=\\?)|(?=\\[)");
+
+
+        for( String token : tokens ){
+            System.out.println("El token es: " + token);
+
+            int lastCharIndex = token.length() - 1;
+            System.out.println("Last char index: " + lastCharIndex );
+
+            String lastChar = token.substring( lastCharIndex );
+
+            switch ( lastChar ) {
+                case "?":
+
+                    boolean agregar = random.nextBoolean();
+
+                    if( agregar )
+                        lista.add( token.substring( 0, lastCharIndex ) );
+                    break;
+
+                case "*":
+
+                    cantidadDeOcurrencias = minimaCantidadDeOcurrencias + ( random.nextInt( maxLength - minimaCantidadDeOcurrencias ) );
+
+                    for( int i = 0; i < cantidadDeOcurrencias; i++ ) {
+                        lista.add( token.substring( 0, lastCharIndex ) );
+                    }
+                    break;
+
+                case "+":
+
+                    cantidadDeOcurrencias = 1 + ( random.nextInt( maxLength ) );
+
+                    for( int i = 0; i < cantidadDeOcurrencias; i++ ) {
+                        lista.add( token.substring( 0, lastCharIndex ) );
+                    }
+                    break;
             }
-        };
+
+            switch ( token ) {
+
+                case "\\":
+                    lista.add("barra");
+                    break;
+                case "[":
+                    lista.add("corchete");
+                    break;
+                case ".":
+                    char caracter = ( char ) random.nextInt(255);
+                    lista.add( String.valueOf( caracter ) );
+                    break;
+                default:
+                    lista.add( token );
+                    break;
+            }
+            System.out.println("Agregue: " + lista.get(lista.size() - 1));
+
+        }
+
+        return lista;
     }
 }
